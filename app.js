9230a8d98af0f4b24f82eab12d2798aceaae31e1
ArdySitio <ardy.ai29@gmail.com>
2020-08-27 20:01:50 +0700

    Pembaharuan app.js

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express')
+const route = require('./routes')
 const app = express()
+const port = 3000
 
 app.set('view engine', 'ejs')
 app.engine('ejs', require('express-ejs-extend'))
 
 app.use(express.urlencoded({ extended: true }))
+
+app.use('/', route)
+
+
+app.listen(port, () => {
+    console.log(`Masuk pak eko di ${port}`);
+})
